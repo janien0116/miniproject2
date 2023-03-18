7e0d64796372eef37ae77c6db2e5a6110d6207de
janela317 <castulojanela@gmail.com>
2023-03-18 16:41:47 +0800

    Configure for GitHub Page

--- config/app.php
+++ config/app.php
@@ -54,7 +54,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    // 'url' => env('APP_URL', 'http://localhost'),
+
+    'url' => 'https://janien0116.github.io',
 
     'asset_url' => env('ASSET_URL'),
 
