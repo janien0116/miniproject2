b608353e60d3740e496d0cbd797ad8ecee1e5884
janela317 <castulojanela@gmail.com>
2023-03-24 12:53:47 +0800

    debug edit tour

--- public/js/edit-tour.js
+++ public/js/edit-tour.js
@@ -14,8 +14,8 @@ SEAT_COUNT.value = document.querySelector('#db-seats').value;
 function priceRecall() {
     let place = DESTINATION.value;
     let baseAmount;
-    // let tourAmount = parseFloat(TOUR_AMOUNT.value)
-    // let seatCount = parseFloat(SEAT_COUNT.value);
+    let tourAmount = parseFloat(TOUR_AMOUNT.value);
+    let seatCount = parseFloat(SEAT_COUNT.value);
 
     if (RADIO_2D.checked) {
         if (place == 'Ilocos') {
@@ -38,9 +38,8 @@ function priceRecall() {
             baseAmount = 4499.00;
         }
     }
-    // tourAmount = baseAmount * seatCount;
-    // return TOUR_AMOUNT.value = tourAmount;
-    return baseAmount;
+    tourAmount = baseAmount * seatCount;
+    return TOUR_AMOUNT.value = tourAmount.toFixed(2);
 }
 
 window.onload = function () {
@@ -50,10 +49,12 @@ window.onload = function () {
 
 let fromDate = new Date(FROM_DATE.value);
 let toDate = new Date(TO_DATE.value);
+
 function getDaysDiff() {
     const diffTime = Math.abs(toDate - fromDate);
     return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 }
+
 function checkRadio() {
     const dayDiff = getDaysDiff();
     if (dayDiff == 3) {
@@ -62,43 +63,28 @@ function checkRadio() {
         RADIO_2D.checked = true;
     }
 }
-let tourAmount = parseFloat(TOUR_AMOUNT.value)
-let seatCount = parseFloat(SEAT_COUNT.value);
+
 RADIO_3D.addEventListener('change', function () {
-    TO_DATE.value = new Date(toDate.setDate(fromDate.getDate() + 3)).toISOString().slice(0, 10);
     updateDBPrice();
-    const newBaseAmount = priceRecall();
-    tourAmount = parseFloat(newBaseAmount) * seatCount;
-    TOUR_AMOUNT.value = tourAmount;
+    TO_DATE.value = new Date(toDate.setDate(fromDate.getDate() + 3)).toISOString().slice(0, 10);
+    priceRecall();
 });
 RADIO_2D.addEventListener('change', function () {
-    TO_DATE.value = new Date(toDate.setDate(fromDate.getDate() + 2)).toISOString().slice(0, 10);
     updateDBPrice();
-    const newBaseAmount = priceRecall();
-    tourAmount = parseFloat(newBaseAmount) * seatCount;
-    TOUR_AMOUNT.value = tourAmount;
+    TO_DATE.value = new Date(toDate.setDate(fromDate.getDate() + 2)).toISOString().slice(0, 10);
+    priceRecall();
 });
 
 function updateDBPrice() {
     const dbPriceInput = document.querySelector('#db-price');
     const dbToDate = document.querySelector('#todate-holder');
-    const dbBookedSeats = document.querySelector('#db-seats');
+    const dbSeats = document.querySelector('#db-seats');
     dbPriceInput.value = TOUR_AMOUNT.value;
     dbToDate.value = TO_DATE.value;
-    dbBookedSeats = SEAT_COUNT.value;
+    dbSeats.value = SEAT_COUNT.value;
 }
 
 TOUR_AMOUNT.addEventListener('change', updateDBPrice);
 BTN_SAVE.addEventListener('click', updateDBPrice);
-SEAT_COUNT.addEventListener('change', function(){
-    // updateDBPrice();
-    const newBaseAmount = priceRecall();
-    tourAmount = parseFloat(newBaseAmount) * seatCount;
-    TOUR_AMOUNT.value = tourAmount;
-});
-SEAT_COUNT.addEventListener('input', function(){
-    // updateDBPrice();
-    const newBaseAmount = priceRecall();
-    tourAmount = parseFloat(newBaseAmount) * seatCount;
-    TOUR_AMOUNT.value = tourAmount;
-});
+SEAT_COUNT.addEventListener('change', priceRecall);
+SEAT_COUNT.addEventListener('input', priceRecall);
