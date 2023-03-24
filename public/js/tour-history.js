9a00230e8b81fc63e3ebd17f5304902cff38a3fb
Alkaid <119391726+aljagape@users.noreply.github.com>
2023-03-24 13:59:03 +0800

    Added other locations
    tour-history.js

--- public/js/tour-history.js
+++ public/js/tour-history.js
@@ -15,8 +15,26 @@ divTourDetails.forEach((tourDetail, index) => {
         destinationImg[index].src = '../images/la-union.jpg';
     } else if (destinationText === 'Baler') {
         destinationImg[index].src = '../images/baler.jpg';
-      } else if (destinationText === 'Intramuros') {
+    } else if (destinationText === 'Intramuros') {
         destinationImg[index].src = '../images/intramuros1.jpg';
+    } else if (destinationText === 'Palawan') {
+        destinationImg[index].src = '../images/palawan.jpg';
+    } else if (destinationText === 'Siargao') {
+        destinationImg[index].src = '../images/siargao.jpg';
+    } else if (destinationText === 'Baguio') {
+        destinationImg[index].src = '../images/baguio2.jpg';
+    } else if (destinationText === 'Bolinao') {
+        destinationImg[index].src = '../images/bolinao2.jpg';
+    } else if (destinationText === 'Batanes') {
+        destinationImg[index].src = '../images/batanes.jpg';
+    } else if (destinationText === 'Burias') {
+        destinationImg[index].src = '../images/burias4.jpg';
+    } else if (destinationText === 'Caramoan') {
+        destinationImg[index].src = '../images/caramoan3.jpg';
+    } else if (destinationText === 'Bohol') {
+        destinationImg[index].src = '../images/bohol.jpg';
+    } else if (destinationText === 'Maniwaya') {
+        destinationImg[index].src = '../images/maniwaya.jpg';
     } else {
       destinationImg[index].src = 'images/destination-placeholder.png';
     }
